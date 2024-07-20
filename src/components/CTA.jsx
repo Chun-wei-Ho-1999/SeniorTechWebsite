1a0bd76a90cd8f45e7bdf8c3d014fd70957803c6
Chun-wei-Ho <chunweiho1999@gmail.com>
2024-07-21 02:10:19 +0800

    📃 Changing wording and tone down animation

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,8 +6,8 @@ const CTA = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.background} ${styles.marginY} ${styles.padding} sm:flex-row flex-col rounded-[20px] box-shadow`}>
       <div className='flex-1 flex flex-col'>
-        <h2 className={styles.heading2}>Let's try our service now!</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        <h3 className={styles.heading3}>Discover the benefits of our AI innovative technology</h3>
+        <p className={`${styles.paragraph} max-w-[1000px] mt-5`}>
           We invite you to collaborate with us in incorporating our cutting-edge AI software into your hardware, aiming to simplify the user experience by removing gel application and reducing skin irritation from adhesive sheets/medical tapes. This innovation will significantly expand your market presence
         </p>
       </div>
